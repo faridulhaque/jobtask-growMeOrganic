20da749cf771c77fe406422876f0435898ddd77f
faridulhaque <faridulhaquemurshed@gmail.com>
2023-12-02 20:09:31 +0600

    done

--- src/components/Expand.tsx
+++ src/components/Expand.tsx
@@ -7,50 +7,124 @@ import ListItemText from "@mui/material/ListItemText";
 import Collapse from "@mui/material/Collapse";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import { Checkbox } from "@mui/material";
 
 type TDepArr = TDepObj[];
 
 type TDepObj = {
+  main: TDepVal;
+  subDep: TDepVal[];
+};
+
+type TDepVal = {
   name: string;
-  subDep: string[];
+  val: number;
 };
 
 const Expand = () => {
   const [open, setOpen] = useState<{ [key: number]: boolean }>({});
+  
+  const [departments, setDepartments] = useState<TDepArr>([
+    {
+      main: {
+        name: "Customer Service",
+        val: 0,
+      },
+      subDep: [
+        {
+          name: "Support",
+          val: 0,
+        },
+        {
+          name: "Customer Success",
+          val: 0,
+        },
+      ],
+    },
+    {
+      main: {
+        name: "Design",
+        val: 0,
+      },
+      subDep: [
+        {
+          name: "Graphics Design",
+          val: 0,
+        },
+        {
+          name: "Web Design",
+          val: 0,
+        },
+        {
+          name: "Product Design",
+          val: 0,
+        },
+      ],
+    },
+  ]);
 
   const handleClick = (index: number) => {
     setOpen((prevOpen) => ({ ...prevOpen, [index]: !prevOpen[index] }));
   };
 
-  const departments: TDepArr = [
-    {
-      name: "Customer Service",
-      subDep: ["Support", "Customer Success"],
-    },
-    {
-      name: "Design",
-      subDep: ["Graphics Design", "Product Design", "Product Design"],
-    },
-  ];
+  const handleSelectOne = (index: number, subIndex: number) => {
+    const updatedDepartments = [...departments];
+
+    if (updatedDepartments[index].subDep[subIndex].val === 0) {
+      updatedDepartments[index].subDep[subIndex].val = 1;
+    } else {
+      updatedDepartments[index].subDep[subIndex].val = 0;
+    }
+
+    const allSubDepsSelected = updatedDepartments[index].subDep.every(
+      (subDep) => subDep.val === 1
+    );
+    updatedDepartments[index].main.val = allSubDepsSelected ? 1 : 0;
+
+    setDepartments(updatedDepartments);
+  };
+
+  const handleSelectAll = (index: number) => {
+    const updatedDepartments = [...departments];
+
+    updatedDepartments[index].main.val =
+      updatedDepartments[index].main.val === 0 ? 1 : 0;
+
+    updatedDepartments[index].subDep.forEach((subDep) => {
+      subDep.val = updatedDepartments[index].main.val;
+    });
+
+    setDepartments(updatedDepartments);
+  };
 
   return (
     <Box component="section" sx={styleThree.section}>
       <List component="div">
         {departments.map((department, index) => (
           <div key={index}>
-            <ListItem onClick={() => handleClick(index)}>
-              <ListItemText primary={department.name} />
+            <ListItem>
+              <Checkbox
+                color="primary"
+                checked={department.main.val === 1}
+                onClick={() => handleSelectAll(index)}
+              />
+              <ListItemText primary={department.main.name} />
               {open[index] ? (
-                <ExpandLessIcon />
+                <ExpandLessIcon onClick={() => handleClick(index)} />
               ) : (
-                <ExpandMoreIcon  />
+                <ExpandMoreIcon onClick={() => handleClick(index)} />
               )}
             </ListItem>
             <Collapse in={open[index]} timeout="auto" unmountOnExit>
               <List component="div" disablePadding>
                 {department.subDep.map((subDepartment, subIndex) => (
                   <ListItem key={subIndex} sx={{ pl: 4 }}>
-                    <ListItemText primary={subDepartment} />
+                    <Checkbox
+                      color="primary"
+                      checked={subDepartment.val === 1}
+                      onClick={() => handleSelectOne(index, subIndex)}
+                    />
+                    <ListItemText primary={subDepartment.name} />
                   </ListItem>
                 ))}
               </List>
